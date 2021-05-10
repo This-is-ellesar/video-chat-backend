591b0fc214d0d36b99a57ff7dc4f423a10932f6a
This-is-ellesar <54068776+This-is-ellesar@users.noreply.github.com>
2021-05-10 17:38:23 +0300

    Добавил документацию к api auth

--- Routers/room-router.js
+++ Routers/room-router.js
@@ -3,10 +3,45 @@ const controller = require('../Controllers/room-controller')
 const { check } = require('express-validator')
 const router = new Router()
 
-
+/**
+ * @swagger
+ * /add_room/:
+ *  post: 
+ *     description: Добавить комнату
+ *     responses: 
+ *      200: 
+ *        description: Success
+ */
 router.post('/add_room/', controller.addRoom)
+/**
+ * @swagger
+ * /rooms/:id/:
+ *  patch: 
+ *     description: Изменить модель комнаты по id
+ *     responses: 
+ *      200: 
+ *        description: Success
+ */
 router.patch('/rooms/:id/', controller.changeRoom)
+/**
+ * @swagger
+ * /rooms/:
+ *  get: 
+ *     description: Получить список всех комнат
+ *     responses: 
+ *      200: 
+ *        description: Success
+ */
 router.get('/rooms/', controller.getRooms)
+/**
+ * @swagger
+ * /users:
+ *  get: 
+ *     description: Получить комнату по id
+ *     responses: 
+ *      200: 
+ *        description: Success
+ */
 router.get('/rooms/:id/', controller.getRoom)
 
 
