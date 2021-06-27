f5531af669fb53adf4bdb40c30817b508158537c
This-is-ellesar <armen16091998@icloud.com>
2021-06-28 00:12:24 +0300

    Форматирование документа

--- Controllers/room-controller.js
+++ Controllers/room-controller.js
@@ -1,99 +1,103 @@
-const Room = require('../models/Room')
-const multer = require('multer')
-const { validationResult } = require('express-validator')
-
+const Room = require('../models/Room');
+const multer = require('multer');
+const { validationResult } = require('express-validator');
 
 class roomController {
-    async getRooms(req, res){
-        try {
-            const rooms = await Room.find()
-            res.status(200).json(rooms)
-        } catch(e) {
-            console.log(e)
-        }
+  async getRooms(req, res) {
+    try {
+      const rooms = await Room.find();
+      res.status(200).json(rooms);
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    async getRoom(req, res){
-        try {
-            const room = await Room.find({ _id: req.params.id })
+  async getRoom(req, res) {
+    try {
+      const room = await Room.find({ _id: req.params.id });
 
-            if(!room) {
-                res.status(422).json({ 
-                    message: 'Комната не найдена!'
-                })
-            }
+      if (!room) {
+        res.status(422).json({
+          message: 'Комната не найдена!',
+        });
+      }
 
-            res.status(200).json(room)
-        } catch (error) {
-            console.log(error)
-        }
+      res.status(200).json(room);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async addRoom(req, res) {
-        try{ 
-            const errors = validationResult(res)
+  async addRoom(req, res) {
+    try {
+      const errors = validationResult(res);
 
-            if(!errors.isEmpty()){ 
-                console.log('Ошибка при создании комнаты: Данные не валидны!')
-            }
-            const { name } = req.body
-            const candidate = await Room.findOne({ name })
+      if (!errors.isEmpty()) {
+        console.log('Ошибка при создании комнаты: Данные не валидны!');
+      }
+      const { name } = req.body;
+      const candidate = await Room.findOne({ name });
 
-            if(candidate) {
-                res.status(400).json('Ошибка при создании комнаты: Комната с таким названием уже существует!')
-            }   
+      if (candidate) {
+        res
+          .status(400)
+          .json(
+            'Ошибка при создании комнаты: Комната с таким названием уже существует!'
+          );
+      }
 
-            if(!name) {
-                res.status(400).json('Название комнаты не валидно!')
-            }
+      if (!name) {
+        res.status(400).json('Название комнаты не валидно!');
+      }
 
-            const room = new Room({
-                name,
-                avatar: null,
-                users: []
-            })
+      const room = new Room({
+        name,
+        avatar: null,
+        users: [],
+      });
 
-            await room.save()
+      await room.save();
 
-            res.status(201).json('Комната успешно создана!')
-        } catch(e){
-            console.log(e)
-        }
+      res.status(201).json('Комната успешно создана!');
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    async changeRoom(req, res) {
-        try {
-            let filedata = req.file
-            let roomId = req.params.id
+  async changeRoom(req, res) {
+    try {
+      let filedata = req.file;
+      let roomId = req.params.id;
 
-            const room = await Room.findOne({
-                _id: roomId
-            })
+      const room = await Room.findOne({
+        _id: roomId,
+      });
 
-            if(!room){
-                res.status(400).json('Ошибка при создании комнаты: Комната с таким названием уже существует!')
-            }   
+      if (!room) {
+        res
+          .status(400)
+          .json(
+            'Ошибка при создании комнаты: Комната с таким названием уже существует!'
+          );
+      }
 
-            if(!filedata) {
-                res.status(400).json({
-                    message: 'Ошибка: Не удалось загрузить файл на сервер!'
-                })
-            }
+      if (!filedata) {
+        res.status(400).json({
+          message: 'Ошибка: Не удалось загрузить файл на сервер!',
+        });
+      }
 
-            room.avatar = req.file.path
+      room.avatar = req.file.path;
 
-            room.save()
+      room.save();
 
-
-            res.status(201).json({
-                message: 'Аватарка успешно добавлена!'
-            })
-        } catch (error) {
-            console.log(error)
-        }
+      res.status(201).json({
+        message: 'Аватарка успешно добавлена!',
+      });
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
-module.exports = new roomController()
-
-
+module.exports = new roomController();
