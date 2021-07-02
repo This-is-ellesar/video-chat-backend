8d2084c62b132786f275eb0d09b4d1d615b7e5ea
This-is-ellesar <armen16091998@icloud.com>
2021-07-02 20:07:39 +0300

    Добавил либу socket.io

--- index.js
+++ index.js
@@ -1,61 +1,63 @@
 //libs
-const express = require('express')
-const cors = require('cors')
-const multer = require('multer')
-const mongoose = require('mongoose')
-const swaggerJsDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const mongoose = require('mongoose');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const io = requird('socket.io');
 //imports
-const authRouter = require('./Routers/auth-router')
-const roomRouter = require('./Routers/room-router')
-const PORT = process.env.PORT || 5000
+const authRouter = require('./Routers/auth-router');
+const roomRouter = require('./Routers/room-router');
+const PORT = process.env.PORT || 5000;
 
-//multer 
+//multer
 const storage = multer.diskStorage({
-    destination: function(req, res, cb){
-        cb(null, './uploads')
-    },
-    filename: function(req, file, cb){
-        cb(null, file.originalname)
-    }
-})
+  destination: function (req, res, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
 
 //swagger
 const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'Video-chat API',
-            description: 'This is my first api for a node.js'
-        },
-        servers: [
-            {
-                url: 'http://localhost:5000/'
-            }
-        ]
+  swaggerDefinition: {
+    info: {
+      title: 'Video-chat API',
+      description: 'This is my first api for a node.js',
     },
-    apis: ['./Routers/room-router.js']
-}
+    servers: [
+      {
+        url: 'http://localhost:5000/',
+      },
+    ],
+  },
+  apis: ['./Routers/room-router.js'],
+};
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions)
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-const app = express()
+const app = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-app.use(multer({ storage: storage }).single('filedata'))
-app.use('./uploads', express.static('uploads'))
-app.use(cors())
-app.use(express.json())
-app.use('/auth', authRouter)
-app.use('/room', roomRouter)
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(multer({ storage: storage }).single('filedata'));
+app.use('./uploads', express.static('uploads'));
+app.use(cors());
+app.use(express.json());
+app.use('/auth', authRouter);
+app.use('/room', roomRouter);
 
 const start = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://Ellesar:Ellesaradmin1@cluster0.rf9a8.mongodb.net/Video-chat?retryWrites=true&w=majority')
-        app.listen(PORT, () => console.warn(`Server started on port ${PORT}`))
-    } catch(e) {
-        console.log(`Server error: ${e}!`)
-    }
-}
+  try {
+    await mongoose.connect(
+      'mongodb+srv://Ellesar:Ellesaradmin1@cluster0.rf9a8.mongodb.net/Video-chat?retryWrites=true&w=majority'
+    );
+    app.listen(PORT, () => console.warn(`Server started on port ${PORT}`));
+  } catch (e) {
+    console.log(`Server error: ${e}!`);
+  }
+};
 
-start()
+start();
